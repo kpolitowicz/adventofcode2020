d516cf0a903ab3266a9949302050447680956769
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-02 20:23:47 -0500

    refac: use only one aswer file with main func

--- 02/answer.go
+++ 02/answer.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
 func main() {
 	dat, _ := ioutil.ReadFile("input.txt")
+
+	validCheckFun := Password.isValid
+	if part := os.Args[1]; part == "2" {
+		validCheckFun = Password.isReallyValid
+	}
+
 	passwords := ParseInput(strings.Trim(string(dat), "\n"))
-	countValid := CountValidPasswords(passwords, Password.isReallyValid)
+	countValid := CountValidPasswords(passwords, validCheckFun)
 
 	fmt.Println(countValid)
 }
