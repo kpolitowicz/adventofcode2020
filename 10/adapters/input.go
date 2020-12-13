e00e9b18442cb695bd07271672766e26dee53883
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-12 22:23:08 -0500

    10/part1 parse input

--- 10/adapters/input.go
+++ 10/adapters/input.go
@@ -1,57 +1,17 @@
-package encoding
+package adapters
 
 import (
 	// "regexp"
+	// "sort"
 	"strconv"
 	"strings"
 )
 
-type XmasData []uint64
+type JoltageAdapters []int
 
-func (d XmasData) FindBadNumber(preambleSize int) (res uint64) {
-	for i := preambleSize; i < len(d); i++ {
-		if !d.goodNumber(i, preambleSize) {
-			return d[i]
-		}
-	}
-
-	return
-}
-
-func (d XmasData) FindSetForWeakness(num uint64) (int, int) {
-	for i := 0; i < len(d)-1; i++ {
-		sum := d[i]
-		for j := i + 1; j < len(d); j++ {
-			sum += d[j]
-			if sum == num {
-				return i, j
-			}
-			if sum > num {
-				break
-			}
-		}
-	}
-
-	return 0, 0
-}
-
-func (d XmasData) goodNumber(index, preambleSize int) bool {
-	num := d[index]
-
-	for i := index - preambleSize; i < index-1; i++ {
-		for j := i + 1; j < index; j++ {
-			if num == d[i]+d[j] {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func ParseInput(input string) (res XmasData) {
+func ParseInput(input string) (res JoltageAdapters) {
 	for _, line := range strings.Split(input, "\n") {
-		num, _ := strconv.ParseUint(line, 10, 64)
+		num, _ := strconv.Atoi(line)
 		res = append(res, num)
 	}
 
