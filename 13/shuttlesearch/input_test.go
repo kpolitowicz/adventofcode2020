07cb90e81a6b77d914203c50ba43b733cd4b56d8
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-13 15:32:46 -0500

    13/part2 parse input (only line 2)

--- 13/shuttlesearch/input_test.go
+++ 13/shuttlesearch/input_test.go
@@ -9,94 +9,9 @@ import (
 var _ = fmt.Println
 
 func TestParseInput(t *testing.T) {
-	got := ParseInput(`35
-20
-15
-25`)
-	want := JoltageAdapters{0, 15, 20, 25, 35, 38}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestCountJoltageDiffs(t *testing.T) {
-	joltages := ParseInput(`16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4`)
-	got := joltages.CountJoltageDiffs()
-	want := map[int]int{
-		1: 7,
-		3: 5,
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestCountChainsExample1(t *testing.T) {
-	joltages := ParseInput(`16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4`)
-	got := joltages.CountChains()
-	want := uint64(8)
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestCountChainsExample2(t *testing.T) {
-	joltages := ParseInput(`28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3`)
-	got := joltages.CountChains()
-	want := uint64(19208)
+	got := ParseInput(`939
+7,13,x,x,59,x,31,19`)
+	want := BusSchedule{7, 13, 0, 0, 59, 0, 31, 19}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
