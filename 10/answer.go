439028304ee81896103c36e33b8ea9551e62fcd3
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-12 22:40:17 -0500

    10/part1 answer

---
+++ 10/answer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kpolitowicz/adventofcode2020/adapters"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	dat, _ := ioutil.ReadFile("input.txt")
+	jolts := adapters.ParseInput(strings.Trim(string(dat), "\n"))
+
+	switch os.Args[1] {
+	case "1":
+		diffs := jolts.CountJoltageDiffs()
+		fmt.Println(diffs[1] * diffs[3])
+	case "2":
+		// program.FixCorruptCommand()
+		// fmt.Println(program.Accumulator)
+	}
+}
