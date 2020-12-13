40048ec1c4306c35629751ffab32fb2cf6923c4c
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-12 22:36:44 -0500

    10/part1 count diffs

--- 10/adapters/input.go
+++ 10/adapters/input.go
@@ -2,12 +2,29 @@ package adapters
 
 import (
 	// "regexp"
-	// "sort"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type JoltageAdapters []int
+
+func (ja JoltageAdapters) CountJoltageDiffs() map[int]int {
+	res := make(map[int]int)
+	sort.Ints(ja)
+
+	prev := 0
+	for _, next := range ja {
+		diff := next - prev
+		res[diff]++
+		prev = next
+	}
+
+	// add diff between last adapter and the device (always 3)
+	res[3]++
+
+	return res
+}
 
 func ParseInput(input string) (res JoltageAdapters) {
 	for _, line := range strings.Split(input, "\n") {
--- 10/adapters/input_test.go
+++ 10/adapters/input_test.go
@@ -19,3 +19,26 @@ func TestParseInput(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestCountJoltageDiffs(t *testing.T) {
+	joltages := ParseInput(`16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`)
+	got := joltages.CountJoltageDiffs()
+	want := map[int]int{
+		1: 7,
+		3: 5,
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
