7312170660175e6cd7227ccffd26bf5edf93ce06
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-13 08:51:25 -0500

    10/part2 counting chain variants

--- 10/adapters/input.go
+++ 10/adapters/input.go
@@ -1,11 +1,13 @@
 package adapters
 
 import (
-	// "fmt"
+	"fmt"
 	"sort"
 	"strconv"
 	"strings"
 )
+
+var _ = fmt.Println
 
 type JoltageAdapters []int
 
@@ -23,6 +25,40 @@ func (ja JoltageAdapters) CountJoltageDiffs() map[int]int {
 	}
 
 	return res
+}
+
+func (ja JoltageAdapters) CountChains() uint64 {
+	res := uint64(1)
+
+	prev := 0
+	currentStreakLen := 0 // streak is the chain of increase-by-1 joltages
+	for _, next := range ja {
+		diff := next - prev
+		prev = next
+		if diff > 1 {
+			res *= ja.combinationsForStreakOf(currentStreakLen)
+			currentStreakLen = 1
+		} else {
+			currentStreakLen++
+		}
+	}
+
+	return res
+}
+
+func (ja JoltageAdapters) combinationsForStreakOf(length int) uint64 {
+	switch length {
+	case 0, 1, 2:
+		return 1
+	case 3:
+		return 2
+	case 4:
+		return 4
+	case 5:
+		return 7
+	default:
+		return 0
+	}
 }
 
 func ParseInput(input string) (res JoltageAdapters) {
--- 10/adapters/input_test.go
+++ 10/adapters/input_test.go
@@ -13,7 +13,7 @@ func TestParseInput(t *testing.T) {
 20
 15
 25`)
-	want := JoltageAdapters{35, 20, 15, 25}
+	want := JoltageAdapters{0, 15, 20, 25, 35, 38}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
@@ -37,6 +37,66 @@ func TestCountJoltageDiffs(t *testing.T) {
 		1: 7,
 		3: 5,
 	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestCountChainsExample1(t *testing.T) {
+	joltages := ParseInput(`16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`)
+	got := joltages.CountChains()
+	want := uint64(8)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestCountChainsExample2(t *testing.T) {
+	joltages := ParseInput(`28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`)
+	got := joltages.CountChains()
+	want := uint64(19208)
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
