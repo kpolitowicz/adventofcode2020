5c2f7f14466c5822d0ee6448f42b69c1eb42e42e
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-13 13:47:31 -0500

    12/ parse input

--- 12/rainrisk/input.go
+++ 12/rainrisk/input.go
@@ -2,74 +2,27 @@ package rainrisk
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 var _ = fmt.Println
 
-type JoltageAdapters []int
-
-func (ja JoltageAdapters) CountJoltageDiffs() map[int]int {
-	res := make(map[int]int)
-
-	prev := 0
-	for _, next := range ja {
-		if next == 0 {
-			continue
-		}
-		diff := next - prev
-		res[diff]++
-		prev = next
-	}
-
-	return res
+type NavigationCmd struct {
+	Cmd byte
+	Arg int
 }
+type NavigationData []NavigationCmd
 
-func (ja JoltageAdapters) CountChains() uint64 {
-	res := uint64(1)
-
-	prev := 0
-	currentStreakLen := 0 // streak is the chain of increase-by-1 joltages
-	for _, next := range ja {
-		diff := next - prev
-		prev = next
-		if diff > 1 {
-			res *= ja.combinationsForStreakOf(currentStreakLen)
-			currentStreakLen = 1
-		} else {
-			currentStreakLen++
-		}
-	}
-
-	return res
-}
-
-func (ja JoltageAdapters) combinationsForStreakOf(length int) uint64 {
-	switch length {
-	case 0, 1, 2:
-		return 1
-	case 3:
-		return 2
-	case 4:
-		return 4
-	case 5:
-		return 7
-	default:
-		return 0
-	}
-}
-
-func ParseInput(input string) (res JoltageAdapters) {
-	res = JoltageAdapters{0}
+func ParseInput(input string) (res NavigationData) {
 	for _, line := range strings.Split(input, "\n") {
-		num, _ := strconv.Atoi(line)
-		res = append(res, num)
+		res = append(res, parseInputLine(line))
 	}
-
-	sort.Ints(res)
-	res = append(res, res[len(res)-1]+3) //add device as +3
 
 	return
+}
+
+func parseInputLine(line string) NavigationCmd {
+	argNum, _ := strconv.Atoi(line[1:])
+	return NavigationCmd{line[0], argNum}
 }
--- 12/rainrisk/input_test.go
+++ 12/rainrisk/input_test.go
@@ -9,94 +9,18 @@ import (
 var _ = fmt.Println
 
 func TestParseInput(t *testing.T) {
-	got := ParseInput(`35
-20
-15
-25`)
-	want := JoltageAdapters{0, 15, 20, 25, 35, 38}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
+	got := ParseInput(`F10
+N3
+F7
+R90
+F11`)
+	want := NavigationData{
+		{'F', 10},
+		{'N', 3},
+		{'F', 7},
+		{'R', 90},
+		{'F', 11},
 	}
-}
-
-func TestCountJoltageDiffs(t *testing.T) {
-	joltages := ParseInput(`16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4`)
-	got := joltages.CountJoltageDiffs()
-	want := map[int]int{
-		1: 7,
-		3: 5,
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestCountChainsExample1(t *testing.T) {
-	joltages := ParseInput(`16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4`)
-	got := joltages.CountChains()
-	want := uint64(8)
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestCountChainsExample2(t *testing.T) {
-	joltages := ParseInput(`28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3`)
-	got := joltages.CountChains()
-	want := uint64(19208)
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
