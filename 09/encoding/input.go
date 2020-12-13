0a5787a7f50af4b248bce3f5ccfd09e2defcc08b
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-12 21:30:48 -0500

    09/part1 finidng first bad num

--- 09/encoding/input.go
+++ 09/encoding/input.go
@@ -8,6 +8,30 @@ import (
 
 type XmasData []uint64
 
+func (d XmasData) FindBadNumber(preambleSize int) (res uint64) {
+	for i := preambleSize; i < len(d); i++ {
+		if !d.goodNumber(i, preambleSize) {
+			return d[i]
+		}
+	}
+
+	return
+}
+
+func (d XmasData) goodNumber(index, preambleSize int) bool {
+	num := d[index]
+
+	for i := index - preambleSize; i < index-1; i++ {
+		for j := i + 1; j < index; j++ {
+			if num == d[i]+d[j] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func ParseInput(input string) (res XmasData) {
 	for _, line := range strings.Split(input, "\n") {
 		num, _ := strconv.ParseUint(line, 10, 64)
--- 09/encoding/input_test.go
+++ 09/encoding/input_test.go
@@ -19,3 +19,61 @@ func TestParseInput(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestFindBAdNumber(t *testing.T) {
+	sequence := ParseInput(`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`)
+	got := sequence.FindBadNumber(5)
+	want := uint64(127)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestGoodNumber(t *testing.T) {
+	sequence := ParseInput(`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`)
+	got := sequence.goodNumber(5, 5)
+	want := true
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
