97f999130f8749ac2b35792b4beda044de4cbd90
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-13 14:37:57 -0500

    12/part2 navigation by waypoint

--- 12/rainrisk/ship.go
+++ 12/rainrisk/ship.go
@@ -5,23 +5,24 @@ type Position struct {
 }
 
 type Ferry struct {
-	Pos    Position
-	Facing int
+	Pos         Position
+	Facing      int
+	WaypointPos Position
 }
 
 type CommandExecutor func(*Ferry, NavigationCmd)
 
 func NewFerry() *Ferry {
 	return &Ferry{
-		Pos:    Position{0, 0},
-		Facing: 90,
+		Pos:         Position{0, 0},
+		Facing:      90,
+		WaypointPos: Position{10, 1},
 	}
 }
 
 func (f *Ferry) ExecuteNavigation(data NavigationData, executor CommandExecutor) {
 	for _, cmd := range data {
 		executor(f, cmd)
-		// f.executeCommand(cmd)
 	}
 }
 
@@ -50,5 +51,36 @@ func (f *Ferry) ExecuteCommand(cmd NavigationCmd) {
 		case 270:
 			f.Pos = Position{f.Pos.X - cmd.Arg, f.Pos.Y}
 		}
+	}
+}
+
+func (f *Ferry) ExecuteWaypointCommand(cmd NavigationCmd) {
+	switch cmd.Cmd {
+	case 'N':
+		f.WaypointPos = Position{f.WaypointPos.X, f.WaypointPos.Y + cmd.Arg}
+	case 'S':
+		f.WaypointPos = Position{f.WaypointPos.X, f.WaypointPos.Y - cmd.Arg}
+	case 'E':
+		f.WaypointPos = Position{f.WaypointPos.X + cmd.Arg, f.WaypointPos.Y}
+	case 'W':
+		f.WaypointPos = Position{f.WaypointPos.X - cmd.Arg, f.WaypointPos.Y}
+	case 'R':
+		f.rotateWaypoint(cmd.Arg)
+	case 'L':
+		rightRotation := (-cmd.Arg + 360) % 360
+		f.rotateWaypoint(rightRotation)
+	case 'F':
+		f.Pos = Position{f.Pos.X + cmd.Arg*f.WaypointPos.X, f.Pos.Y + cmd.Arg*f.WaypointPos.Y}
+	}
+}
+
+func (f *Ferry) rotateWaypoint(degrees int) {
+	switch degrees {
+	case 90:
+		f.WaypointPos = Position{f.WaypointPos.Y, -f.WaypointPos.X}
+	case 180:
+		f.WaypointPos = Position{-f.WaypointPos.X, -f.WaypointPos.Y}
+	case 270:
+		f.WaypointPos = Position{-f.WaypointPos.Y, f.WaypointPos.X}
 	}
 }
--- 12/rainrisk/ship_test.go
+++ 12/rainrisk/ship_test.go
@@ -32,6 +32,24 @@ func TestExecuteNavigation(t *testing.T) {
 	}
 }
 
+func TestExecuteNavigationByWaypoint(t *testing.T) {
+	naviData := NavigationData{
+		{'F', 10},
+		{'N', 3},
+		{'F', 7},
+		{'R', 90},
+		{'F', 11},
+	}
+	ferry := NewFerry()
+	ferry.ExecuteNavigation(naviData, (*Ferry).ExecuteWaypointCommand)
+
+	gotPos := ferry.Pos
+	wantPos := Position{214, -72}
+	if !reflect.DeepEqual(gotPos, wantPos) {
+		t.Errorf("got %v want %v", gotPos, wantPos)
+	}
+}
+
 func TestExecuteCommandN(t *testing.T) {
 	cmd := NavigationCmd{'N', 10}
 	ferry := NewFerry()
@@ -155,5 +173,77 @@ func TestExecuteCommandF(t *testing.T) {
 	wantFace := 90
 	if !reflect.DeepEqual(gotFace, wantFace) {
 		t.Errorf("got %v want %v", gotFace, wantFace)
+	}
+}
+
+func TestExecuteWaypointCommandN(t *testing.T) {
+	cmd := NavigationCmd{'N', 10}
+	ferry := NewFerry()
+	ferry.ExecuteWaypointCommand(cmd)
+
+	gotPos := ferry.Pos
+	wantPos := Position{0, 0}
+	if !reflect.DeepEqual(gotPos, wantPos) {
+		t.Errorf("got %v want %v", gotPos, wantPos)
+	}
+
+	gotWpPos := ferry.WaypointPos
+	wantWpPos := Position{10, 11}
+	if !reflect.DeepEqual(gotWpPos, wantWpPos) {
+		t.Errorf("got %v want %v", gotWpPos, wantWpPos)
+	}
+}
+
+func TestExecuteWaypointCommandF(t *testing.T) {
+	cmd := NavigationCmd{'F', 10}
+	ferry := NewFerry()
+	ferry.ExecuteWaypointCommand(cmd)
+
+	gotPos := ferry.Pos
+	wantPos := Position{100, 10}
+	if !reflect.DeepEqual(gotPos, wantPos) {
+		t.Errorf("got %v want %v", gotPos, wantPos)
+	}
+
+	gotWpPos := ferry.WaypointPos
+	wantWpPos := Position{10, 1}
+	if !reflect.DeepEqual(gotWpPos, wantWpPos) {
+		t.Errorf("got %v want %v", gotWpPos, wantWpPos)
+	}
+}
+
+func TestExecuteWaypointCommandR(t *testing.T) {
+	cmd := NavigationCmd{'R', 90}
+	ferry := NewFerry()
+	ferry.ExecuteWaypointCommand(cmd)
+
+	gotPos := ferry.Pos
+	wantPos := Position{0, 0}
+	if !reflect.DeepEqual(gotPos, wantPos) {
+		t.Errorf("got %v want %v", gotPos, wantPos)
+	}
+
+	gotWpPos := ferry.WaypointPos
+	wantWpPos := Position{1, -10}
+	if !reflect.DeepEqual(gotWpPos, wantWpPos) {
+		t.Errorf("got %v want %v", gotWpPos, wantWpPos)
+	}
+}
+
+func TestExecuteWaypointCommandL(t *testing.T) {
+	cmd := NavigationCmd{'L', 90}
+	ferry := NewFerry()
+	ferry.ExecuteWaypointCommand(cmd)
+
+	gotPos := ferry.Pos
+	wantPos := Position{0, 0}
+	if !reflect.DeepEqual(gotPos, wantPos) {
+		t.Errorf("got %v want %v", gotPos, wantPos)
+	}
+
+	gotWpPos := ferry.WaypointPos
+	wantWpPos := Position{-1, 10}
+	if !reflect.DeepEqual(gotWpPos, wantWpPos) {
+		t.Errorf("got %v want %v", gotWpPos, wantWpPos)
 	}
 }
