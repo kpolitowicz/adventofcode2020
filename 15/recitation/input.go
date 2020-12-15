af5ff9b0d78c9747af3e4ea59055d61d327a9920
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-15 13:50:34 -0500

    15/ init

---
+++ 15/recitation/input.go
@@ -0,0 +1,23 @@
+package recitation
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var _ = fmt.Println
+
+func ParseInput(input string) (res JoltageAdapters) {
+	res = JoltageAdapters{0}
+	for _, line := range strings.Split(input, "\n") {
+		num, _ := strconv.Atoi(line)
+		res = append(res, num)
+	}
+
+	sort.Ints(res)
+	res = append(res, res[len(res)-1]+3) //add device as +3
+
+	return
+}
---
+++ 15/recitation/input_test.go
@@ -0,0 +1,21 @@
+package recitation
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+var _ = fmt.Println
+
+func TestParseInput(t *testing.T) {
+	got := ParseInput(`35
+20
+15
+25`)
+	want := JoltageAdapters{0, 15, 20, 25, 35, 38}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
