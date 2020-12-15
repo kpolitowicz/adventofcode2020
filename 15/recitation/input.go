cf5c3074efd4a395e4e35094ab21d68873f823f5
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-15 14:16:40 -0500

    15/part 1 taking turns

--- 15/recitation/input.go
+++ 15/recitation/input.go
@@ -14,6 +14,27 @@ type MemoryGame struct {
 	LastSpoken map[int]int
 }
 
+func (g *MemoryGame) TakeTurnsUntil(iteration int) {
+	for len(g.Numbers) < iteration {
+		g.NextTurn()
+	}
+}
+
+func (g *MemoryGame) NextTurn() {
+	prevTurn := len(g.Numbers) - 1
+	prevNum := g.Numbers[prevTurn]
+	if lastSpoken, exists := g.LastSpoken[prevNum]; exists && lastSpoken < prevTurn {
+		g.Numbers = append(g.Numbers, prevTurn-lastSpoken)
+	} else {
+		g.Numbers = append(g.Numbers, 0)
+	}
+	g.LastSpoken[prevNum] = prevTurn
+}
+
+func (g *MemoryGame) LastNumber() int {
+	return g.Numbers[len(g.Numbers)-1]
+}
+
 func ParseInput(input string) *MemoryGame {
 	res := MemoryGame{
 		Numbers:    []int{},
--- 15/recitation/input_test.go
+++ 15/recitation/input_test.go
@@ -8,6 +8,94 @@ import (
 
 var _ = fmt.Println
 
+func TestTakeTurnsUntilExample1(t *testing.T) {
+	game := &MemoryGame{
+		Numbers: []int{0, 3, 6},
+		LastSpoken: map[int]int{
+			0: 0,
+			3: 1,
+			6: 2,
+		},
+	}
+	game.TakeTurnsUntil(2020)
+
+	got := game.LastNumber()
+	want := 436
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestTakeTurnsUntilExample7(t *testing.T) {
+	game := ParseInput("3,1,2")
+	game.TakeTurnsUntil(2020)
+
+	got := game.LastNumber()
+	want := 1836
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestTakeTurnsUntilExample2(t *testing.T) {
+	game := ParseInput("1,3,2")
+	game.TakeTurnsUntil(2020)
+
+	got := game.LastNumber()
+	want := 1
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestNextTurnNotSpokenBefore(t *testing.T) {
+	game := &MemoryGame{
+		Numbers: []int{0, 3, 6},
+		LastSpoken: map[int]int{
+			0: 0,
+			3: 1,
+			6: 2,
+		},
+	}
+	game.NextTurn()
+
+	want := &MemoryGame{
+		Numbers: []int{0, 3, 6, 0},
+		LastSpoken: map[int]int{
+			0: 0,
+			3: 1,
+			6: 2,
+		},
+	}
+	if !reflect.DeepEqual(game, want) {
+		t.Errorf("got %v want %v", game, want)
+	}
+}
+
+func TestNextTurnSpokenBefore(t *testing.T) {
+	game := &MemoryGame{
+		Numbers: []int{0, 3, 6, 0},
+		LastSpoken: map[int]int{
+			0: 0,
+			3: 1,
+			6: 2,
+		},
+	}
+	game.NextTurn()
+
+	want := &MemoryGame{
+		Numbers: []int{0, 3, 6, 0, 3},
+		LastSpoken: map[int]int{
+			0: 3,
+			3: 1,
+			6: 2,
+		},
+	}
+	if !reflect.DeepEqual(game, want) {
+		t.Errorf("got %v want %v", game, want)
+	}
+}
+
 func TestParseInput(t *testing.T) {
 	got := ParseInput("0,3,6")
 	want := &MemoryGame{
