bdce29d60d8089a0b266cb3a30620446872e4b2f
Kamil Politowicz <k.politowicz@gmail.com>
2020-12-14 19:44:21 -0500

    14/part2 generate all floating combinations

--- 14/dockingdata/computer.go
+++ 14/dockingdata/computer.go
@@ -2,6 +2,7 @@ package dockingdata
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -73,21 +74,48 @@ func (c *Computer) AndMask() uint64 {
 }
 
 func (c *Computer) AllFloating(num uint64) (res []uint64) {
-	strMask := c.bitMaskFor(num)
-	fmt.Println(strMask)
+	strMask, indexesOfXses := c.bitMaskFor(num)
+	combinations := c.allFloatingCombinations(len(indexesOfXses))
+	for _, variant := range combinations {
+		str := ""
+		currentX := 0
+		for _, ch := range strMask {
+			if ch == 'X' {
+				str += string(variant[currentX])
+				currentX++
+			} else {
+				str += string(ch)
+			}
+		}
+		variantNum, _ := strconv.ParseUint(str, 2, 64)
+		res = append(res, variantNum)
+	}
 
 	return
 }
 
-func (c *Computer) bitMaskFor(num uint64) (res string) {
+func (c *Computer) bitMaskFor(num uint64) (res string, idxX []int) {
 	strMask := fmt.Sprintf("%036s", strconv.FormatUint(num, 2))
 	for idx, ch := range c.CurrentMask {
 		switch ch {
 		case '0':
 			res += string(strMask[idx])
-		case '1', 'X':
-			res += string(ch)
+		case '1':
+			res += "1"
+		case 'X':
+			res += "X"
+			idxX = append(idxX, idx)
 		}
+	}
+
+	return
+}
+
+func (c *Computer) allFloatingCombinations(num int) (res []string) {
+	for n := uint64(0); float64(n) < math.Pow(2, float64(num)); n++ {
+		numStr := strconv.FormatInt(int64(num), 10)
+		nStr := fmt.Sprintf("%0"+numStr+"s", strconv.FormatUint(n, 2))
+		res = append(res, nStr)
 	}
 
 	return
