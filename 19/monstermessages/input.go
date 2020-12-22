631c3f7b049da10eb9d78fbaab053bae2ac2ed62
Kamil Politowicz <kamilp@hey.com>
2020-12-22 19:05:13 +0000

    19/part1 resolving rules

--- 19/monstermessages/input.go
+++ 19/monstermessages/input.go
@@ -5,6 +5,32 @@ import (
 )
 
 type Rules map[string]string
+
+func (r Rules) ResolveRule(rule string) (res string) {
+	ruleStr := r[rule]
+
+	switch {
+	case strings.ContainsRune(ruleStr, '"'):
+		res = string(ruleStr[1])
+	case strings.ContainsRune(ruleStr, '|'):
+		var resParts []string
+		for _, part := range strings.Split(ruleStr, " | ") {
+			resParts = append(resParts, r.resolveSequence(part))
+		}
+		res = "(" + strings.Join(resParts, "|") + ")"
+	default:
+		res = r.resolveSequence(ruleStr)
+	}
+	return
+}
+
+func (r Rules) resolveSequence(str string) (res string) {
+	for _, rule := range strings.Split(str, " ") {
+		res += r.ResolveRule(rule)
+	}
+	return
+}
+
 type Messages []string
 
 func ParseInput(input string) (Rules, Messages) {
--- 19/monstermessages/input_test.go
+++ 19/monstermessages/input_test.go
@@ -5,6 +5,59 @@ import (
 	"testing"
 )
 
+func TestResolveRuleSimple(t *testing.T) {
+	rules := Rules{
+		"0": "4 1 5",
+		"1": "2 3 | 3 2",
+		"2": "4 4 | 5 5",
+		"3": "4 5 | 5 4",
+		"4": "\"a\"",
+		"5": "\"b\"",
+	}
+	got := rules.ResolveRule("5")
+	want := "b"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+func TestResolveRuleOr(t *testing.T) {
+	rules := Rules{
+		"0": "4 1 5",
+		"1": "2 3 | 3 2",
+		"2": "4 4 | 5 5",
+		"3": "4 5 | 5 4",
+		"4": "\"a\"",
+		"5": "\"b\"",
+	}
+	got := rules.ResolveRule("3")
+	want := "(ab|ba)"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestResolveRuleZero(t *testing.T) {
+	rules := Rules{
+		"0": "4 1 5",
+		"1": "2 3 | 3 2",
+		"2": "4 4 | 5 5",
+		"3": "4 5 | 5 4",
+		"4": "\"a\"",
+		"5": "\"b\"",
+	}
+	got := rules.ResolveRule("0")
+	rule2 := "(aa|bb)"
+	rule3 := "(ab|ba)"
+	rule1 := "(" + rule2 + rule3 + "|" + rule3 + rule2 + ")"
+	want := "a" + rule1 + "b"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
 func TestParseInput(t *testing.T) {
 	gotRules, gotMsg := ParseInput(`0: 4 1 5
 1: 2 3 | 3 2
